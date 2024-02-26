e2cc394fc5445bf484fb4302ef54db87ce33d689
Patrick <lteloladeira@gmail.com>
2024-02-25 21:10:21 -0300

    filter

--- ML/Neural_Network/Arrays.py
+++ ML/Neural_Network/Arrays.py
@@ -1,5 +1,16 @@
 import skimage.data
+import numpy as np
 
 img = skimage.data.chelsea()
 
 img = skimage.color.rgb2gray(img)
+
+filter = np.zeros((2, 3, 3)) #Two filters with 3x3 dimmensions
+
+filter[0, :, :] = [[1, 1, 1],
+                   [0, 0, 0], 
+                   [-1, -1, -1]]
+
+filter[1, :, :] = [[1, 0, -1],
+                   [1, 0, -1], 
+                   [1, 0, -1]]
